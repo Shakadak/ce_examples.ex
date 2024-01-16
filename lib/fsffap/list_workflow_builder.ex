e10cedeef5f5528ebe9c562107e923c1e50e0064
Nathanael <Shakadak@users.noreply.github.com>
2024-01-16 20:40:27 +0100

    progressing through combine

--- lib/fsffap/list_workflow_builder.ex
+++ lib/fsffap/list_workflow_builder.ex
@@ -4,6 +4,33 @@ defmodule ListWorkflowBuilder do
   end
 
   def _Pure(x), do: [x]
+
+  def _PureFrom(m), do: m
+
+  def _Zero do
+    :ok = IO.puts("Zero")
+    []
+  end
+
+  def _Yield(x) do
+    :ok = IO.puts("Yield an unwrapped #{inspect(x)} as a list")
+    [x]
+  end
+
+  def _YieldFrom(m) do
+    :ok = IO.puts("Yield a list (#{inspect(m)}) directly")
+    m
+  end
+
+  def _Combine(a, b) do
+    :ok = IO.puts("combining #{inspect(a)} and #{inspect(b)}")
+    Enum.concat([a, b])
+  end
+
+  def _Delay(f) do
+    :ok = IO.puts("Delay")
+    f.()
+  end
 end
 
 defmodule ListExample do
@@ -27,5 +54,51 @@ defmodule ListExample do
       pure i * j
     end
     |> IO.inspect(label: "multiplied=")
+  end
+
+  def ex1 do
+    compute List do
+      yield 1
+      yield 2
+    end
+    |> IO.inspect(label: "Result from yield then yield")
+  end
+
+  def ex2 do
+    compute List do
+      yield 1
+      yield! [2, 3]
+    end
+    |> IO.inspect(label: "Result for yield then yield!")
+  end
+
+  def ex3 do
+    compute List do
+      let! i = ["red", "blue"]
+      yield i
+      let! j = ["hat", "tie"]
+      yield! [i <> " " <> j, "-"]
+    end
+    |> IO.inspect(label: "Result for for..in..do")
+  end
+
+  def ex3_1 do
+    compute List do
+      let! i = ["red", "blue"]
+      pure i
+      let! j = ["hat", "tie"]
+      pure! [i <> " " <> j, "-"]
+    end
+    |> IO.inspect(label: "Result for for..in..do")
+  end
+
+  def ex4 do
+    compute List do
+      yield 1
+      yield 2
+      yield 3
+      yield 4
+    end
+    |> IO.inspect(label: "Result for yield × 4")
   end
 end
--- lib/fsffap/trace_builder.ex
+++ lib/fsffap/trace_builder.ex
@@ -33,6 +33,31 @@ defmodule Trace do
     :ok = IO.puts("Yield an option #{inspect(x)} directly")
     some(x)
   end
+
+  def _Combine(a, b) do
+    case {a, b} do
+      {some(a1), some(b1)} ->
+        :ok = IO.puts("combining #{inspect(a1)} and #{inspect(b1)}")
+        some(a1 + b1)
+
+      {some(a1), none()} ->
+        :ok = IO.puts("combining #{inspect(a1)} with None")
+        some(a1)
+
+      {none(), some(b1)} ->
+        :ok = IO.puts("combining None with #{inspect(b1)}")
+        some(b1)
+
+      {none(), none()} ->
+        :ok = IO.puts("combining None with None")
+        none()
+    end
+  end
+
+  def _Delay(f) do
+    :ok = IO.puts("Delay")
+    f.()
+  end
 end
 
 defmodule TraceExample do
@@ -107,5 +132,55 @@ defmodule TraceExample do
       yield! some(1)
     end
     |> IO.inspect(label: "Result for yield!")
+  end
+
+  def ex10 do
+    compute Trace do
+      yield 1
+      yield 2
+    end
+    |> IO.inspect(label: "Result for yield then yield")
+  end
+
+  def ex11 do
+    compute Trace do
+      pure 1
+      pure 2
+    end
+    |> IO.inspect(label: "Result for pure then pure")
+  end
+
+  def ex12 do
+    compute Trace do
+      if true, do: IO.puts("hello")
+      pure 1
+    end
+    |> IO.inspect(label: "Result for if then pure")
+  end
+
+  def ex13 do
+    compute Trace do
+      yield 1
+      let! _x = none()
+      yield 2
+    end
+    |> IO.inspect(label: "Result for yield then None")
+  end
+
+  def ex14 do
+    compute Trace do
+      yield 1
+      yield 2
+      yield 3
+    end
+    |> IO.inspect(label: "Result for yield × 3")
+  end
+
+  def ex15 do
+    compute Trace do
+      yield 1
+      pure 2
+    end
+    |> IO.inspect(label: "Result for yield then pure")
   end
 end
