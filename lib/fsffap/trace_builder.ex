4680d432721ca8078e55b8be916c8c20b66d2b9c
Nathanael <Shakadak@users.noreply.github.com>
2024-01-16 13:59:49 +0100

    finished Zero and Yield, ignored For

--- lib/fsffap/trace_builder.ex
+++ lib/fsffap/trace_builder.ex
@@ -1,4 +1,4 @@
-defmodule TraceBuilder do
+defmodule Trace do
   import Option
 
   def _Bind(m, f) do
@@ -18,6 +18,21 @@ defmodule TraceBuilder do
     :ok = IO.puts("Returning an option (#{inspect(m)}) directly")
     m
   end
+
+  def _Zero do
+    :ok = IO.puts("Zero")
+    none()
+  end
+
+  def _Yield(x) do
+    :ok = IO.puts("Yield an unwrapped #{inspect(x)} as an option")
+    some(x)
+  end
+
+  def _YieldFrom(x) do
+    :ok = IO.puts("Yield an option #{inspect(x)} directly")
+    some(x)
+  end
 end
 
 defmodule TraceExample do
@@ -25,20 +40,20 @@ defmodule TraceExample do
   import Option
 
   def ex1 do
-    compute TraceBuilder do
+    compute Trace do
       pure 1
     end
     |> IO.inspect(label: "Result 1")
   end
 
   def ex2 do
-    compute TraceBuilder do
+    compute Trace do
       pure! some(2)
     end
   end
 
   def ex3 do
-    compute TraceBuilder do
+    compute Trace do
       let! x = some(1)
       let! y = some(2)
       pure x + y
@@ -47,7 +62,7 @@ defmodule TraceExample do
   end
 
   def ex4 do
-    compute TraceBuilder do
+    compute Trace do
       let! x = none()
       let! y = some(1)
       pure x + y
@@ -57,7 +72,7 @@ defmodule TraceExample do
 
   def ex5 do
     void = fn _ -> {} end
-    compute TraceBuilder do
+    compute Trace do
       do! some void.(IO.puts("...expression that returns unit"))
       do! some void.(IO.puts("...another expression that returns unit"))
       let! x = some 1
@@ -67,16 +82,30 @@ defmodule TraceExample do
   end
 
   def ex6 do
-    compute TraceBuilder do
+    compute Trace do
       IO.puts("Hello world")
     end
     |> IO.inspect(label: "Result for simple expression")
   end
 
   def ex7 do
-    compute TraceBuilder do
-      if false, do: pure 1
+    compute Trace do
+      if false do pure 1 end
     end
     |> IO.inspect(label: "Result for if without else")
+  end
+
+  def ex8 do
+    compute Trace do
+      yield 1
+    end
+    |> IO.inspect(label: "Result for yield")
+  end
+
+  def ex9 do
+    compute Trace do
+      yield! some(1)
+    end
+    |> IO.inspect(label: "Result for yield!")
   end
 end
